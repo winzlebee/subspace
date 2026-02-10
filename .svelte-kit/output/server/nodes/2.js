ed8801edce75406ab1f5105b4210d5177fd0419a
Win Holzapfel <winfried@verbotics.com>
2026-02-10 22:59:03 +1100

    Remove .svelte-kit

--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.CZFiekkj.js","_app/immutable/chunks/Dy-qOuCo.js","_app/immutable/chunks/jPXpONmM.js","_app/immutable/chunks/bMZSfw0A.js","_app/immutable/chunks/C5ABWx1_.js","_app/immutable/chunks/QhsLRqcb.js","_app/immutable/chunks/Ci1e0nyO.js","_app/immutable/chunks/B_v7Z973.js"];
-export const stylesheets = [];
-export const fonts = [];
